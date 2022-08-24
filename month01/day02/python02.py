2b2f162fa4e96f297c89e113fc9beb8a84ea8562
wangqiang91 <786528604@qq.com>
2022-08-24 17:58:50 +0800

    day02上午

---
+++ month01/day02/python02.py
@@ -0,0 +1,16 @@
+'''
+    函数 print
+'''
+print("ce shi", end=" ")
+
+print("a huan")
+
+'''
+    函数 input
+'''
+# name = input("请输入姓名：")
+# print("您好，" + name)
+subject = input("请输入i kiss you的主语：")
+predicate = input("请输入i kiss you的主语：")
+object = input("请输入i kiss you的主语：")
+print("您输入的主语是：{}，谓语是：{}，宾语是：{}".format(subject, predicate, object))
