9f7137580c1b416471dca04a327f5bd4199e64c3
wangqiang91 <786528604@qq.com>
2022-09-13 17:53:12 +0800

    python

--- month01/day02/python06.py
+++ month01/day02/python06.py
@@ -1,3 +1,27 @@
 """
     bool运算、比较运算、逻辑运算
 """
+
+
+# print(int(input('输入的是正数')) > 0)
+
+# print(1 <= int(input('输入的是月份')) <= 12)
+
+# print(int(input('输入的不是偶数')) % 2 != 0)
+
+
+# data01 = 100
+# data02 = 200
+# data03 = data01 + data02
+# data01 = 200
+# print(data03)
+
+bridegroom_name = "武大郎"
+bride_name = "潘金莲"
+temp = bridegroom_name
+bridegroom_name = bride_name
+bride_name = temp
+print("交换后的新郎：" + bridegroom_name)  
+print("交换后的新娘：" + bride_name)
+
+
---
+++ month01/day03/demo02.py
@@ -0,0 +1,42 @@
+'''
+    选择语句, if 
+'''
+
+# sex = input("请输入性别")
+# if sex == "男":
+#     print("您好，先生！")
+# elif sex == "女":
+#     print("您好，女士！")
+# else:
+#     print("性别输入错误")
+
+
+# number1 = int(input("请输入身高："))
+# number2 = int(input("请输入身高："))
+# number3 = int(input("请输入身高："))
+# number4 = int(input("请输入身高："))
+# max_number = number1
+# if max_number < number2:
+#     max_number = number2
+# if max_number < number3:
+#     max_number = number3
+# if max_number < number4:
+#     max_number = number4
+# print(max_number)
+
+#连续区间判断，可以只考虑单边;
+ma = int(input("请输入心里年龄："))
+ca = int(input("请输入实际年龄："))
+iq = ma / ca * 100
+if iq >= 140:
+    print("天才")
+elif iq >= 120:
+    print("聪慧")
+elif iq >= 110:
+    print("正常")
+elif iq >= 90:
+    print("")
+elif iq >= 80:
+    print("迟钝")
+else:
+    print("低能")
