285102c989115e4c64611e7c4c5087f154b0607b
wangqiang91 <786528604@qq.com>
2022-09-14 17:58:37 +0800

    python

--- month01/day03/demo02.py
+++ month01/day03/demo02.py
@@ -25,18 +25,31 @@
 # print(max_number)
 
 #连续区间判断，可以只考虑单边;
-ma = int(input("请输入心里年龄："))
-ca = int(input("请输入实际年龄："))
-iq = ma / ca * 100
-if iq >= 140:
-    print("天才")
-elif iq >= 120:
-    print("聪慧")
-elif iq >= 110:
-    print("正常")
-elif iq >= 90:
-    print("")
-elif iq >= 80:
-    print("迟钝")
+# ma = int(input("请输入心里年龄："))
+# ca = int(input("请输入实际年龄："))
+# iq = ma / ca * 100
+# if iq >= 140:
+#     print("天才")
+# elif iq >= 120:
+#     print("聪慧")
+# elif iq >= 110:
+#     print("正常")
+# elif iq >= 90:
+#     print("")
+# elif iq >= 80:
+#     print("迟钝")
+# else:
+#     print("低能")
+
+
+
+month_number = int(input("请输入月份："))
+if 1 <= month_number <= 12:
+    if month_number == 2:
+        print("29天")
+    elif month_number in (4,6,9,11):
+        print("30天")
+    else:
+        print("31天")
 else:
-    print("低能")
+    print("月份有误")
