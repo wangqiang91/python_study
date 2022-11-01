0966f7d5b4e930d5e88a3523880fd325e6b578fb
wangqiang91 <786528604@qq.com>
2022-11-01 18:23:00 +0800

    python

--- month01/day06_dict/demo05.py
+++ month01/day06_dict/demo05.py
@@ -2,13 +2,49 @@
     练习
 '''
 
-dict_travel_info = {
-    "北京":{
-        "景区":["长城","圆明园"],
-        "美事":[]
-    },
-    "四川":{
-        "景区":[],
-        "美事":[]
-    }
+# dict_travel_info = {
+#     "北京":{
+#         "景区":["长城","故宫"],
+#         "美食":["烤鸭","豆汁胶圈","杂酱面"]
+#     },
+#     "四川":{
+#         "景区":["九寨沟","峨眉山"],
+#         "美食":["火锅","兔头"]
+#     }
+# }
+
+# print(dict_travel_info["北京"]["景区"][0])
+
+# print(dict_travel_info["四川"]["美食"][1])
+
+# for key in dict_travel_info.keys():
+#     print(key)
+
+# for food in dict_travel_info["北京"]["美食"]:
+#     print(food)
+# print("++++++++++++++++++++++++++")
+
+# for city in dict_travel_info.keys():
+#     for food in dict_travel_info[city]["美食"]:
+#         print(food)
+from unicodedata import name
+
+
+dicy_commodity_infos = {
+    1001:{"name":"屠龙刀","price":10000},
+    1002:{"name":"倚天剑","price":10000},
+    1003:{"name":"金箍棒","price":52000},
+    1004:{"name":"口罩","price":20},
+    1005:{"name":"酒精","price":30}
 }
+list_orders = [
+    {"cid":1001,"count":1},
+    {"cid":1002,"count":3},
+    {"cid":1005,"count":2}
+]
+for item in list_orders:
+    print(f"商品编号{item['cid']},购买数量{item['count']}")
+
+for item in list_orders:
+    cid = dicy_commodity_infos[item["cid"]]
+    print(f"商品名称{cid['name']},商品单价{cid['price']},数量{item['count']}")
---
+++ month01/day06_dict/demo06.py
@@ -0,0 +1,45 @@
+'''
+    练习
+'''
+
+colour = {"R":"红色","G":"绿色","B":"蓝色","A":"透明度"}
+data = input("请输入颜色")
+if data in colour.keys():
+    print(colour[data])
+else:
+    print("颜色无效！")
+
+list02 = [5,1,4,6,7,4,6,8,5]
+count = list02[0]
+for i in range(1,len(list02)):
+    count -= list02[i]
+print(count)
+
+dict_employees = {
+    1001: {"did": 9002, "name": "师父", "money": 60000},
+    1002: {"did": 9001, "name": "孙悟空", "money": 50000},
+    1003: {"did": 9002, "name": "猪八戒", "money": 20000},
+    1004: {"did": 9001, "name": "沙僧", "money": 30000},
+    1005: {"did": 9001, "name": "小白龙", "money": 15000},
+}
+
+for sid,message in dict_employees.items():
+    print(f"{message['name']}的员工编号是{sid}，部门编号是{message['did']}，月薪是{message['money']}元；")
+
+for sid,message in dict_employees.items():
+    if message['money'] > 20000:
+        print(f"{message['name']}的员工编号是{sid}，部门编号是{message['did']}，月薪是{message['money']}元；")
+
+import random
+list_red = []
+blue = random.randint(1,16)
+while True:
+    num = random.randint(1,33)
+    if num not in list_red:
+        list_red.append(num)
+    if len(list_red) == 6:
+        break
+print(list_red)
+for item in list_red:
+    print(item,end="")
+print(blue)
