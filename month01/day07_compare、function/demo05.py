1ea4c8818f9b18ea7f404555f12f67835d1c55a6
wangqiang91 <786528604@qq.com>
2022-11-12 18:36:05 +0800

    python

--- month01/day07_compare、function/demo05.py
+++ month01/day07_compare、function/demo05.py
@@ -43,3 +43,24 @@ print(get_social_insurance(5000))
 print(get_discount("vip",900))
 print(get_list_min([3,5,3,32,4,23,-4,6,34]))
 print(get_stage_of_life(80))
+
+def get_descending_order(list_target):
+    for i in range(len(list_target)-1):
+        for j in range(i+1,len(list_target)):
+            if list_target[i] < list_target[j]:
+                list_target[i],list_target[j] = list_target[j],list_target[i]
+list1 = [5,15,25,35,1,2]
+get_descending_order(list1)
+print(list1) 
+
+def compare_and_set_none(list_target,num):
+    list_res = []
+    for item in list_target:
+        if item > num:
+            item = None
+        list_res.append(item)
+    return list_res
+list01 = [34,545,56,7,78,8]
+print(compare_and_set_none(list01,10))
+print(compare_and_set_none(list01,100))
+
