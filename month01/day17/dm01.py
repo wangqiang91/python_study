3be0a4d104dcef6cf80bc1ae80286e533c5bee49
unknown <786528604@qq.com>
2023-04-20 21:58:12 +0800

    lambda

---
+++ month01/day17/dm01.py
@@ -0,0 +1,31 @@
+"""
+    lambda(匿名方法)
+"""
+def func01(p1,p2):
+    return p1 > p2
+print(func01(3,5))
+# 对应的匿名方法
+data1 = lambda p1,p2:p1 > p2
+print(data1(3,5))
+
+
+def func02(p1):
+    print("返回值是：",p1)
+func02(230)
+# 对应的匿名方法
+data2 = lambda p1 : print("返回值是：",p1)
+data2(230)
+
+
+def fun03():
+    return 100
+print(fun03())
+# 对应的匿名方法
+data3 = lambda : 100
+print(data3())
+
+
+def func04():
+    for i in range(20):
+        print(i)
+# data3 = lambda : for i in range(20) : print(i)  #错误的写法，lambda函数只能有一条语句；
