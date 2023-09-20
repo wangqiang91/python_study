4869c479eeccdbbdcd6ffb43ee8de4cf46674bfd
wangqiang91 <786528604@qq.com>
2023-09-20 19:07:31 +0800

    级联动作

--- month02/day12/demo02_约束.sql
+++ month02/day12/demo02_约束.sql
@@ -28,3 +28,34 @@ alter table person add FOREIGN KEY (dept_id) REFERENCES dept(id);
 
 SHOW CREATE TABLE person;
 show index from person;
+
+alter Table person DROP FOREIGN KEY person_ibfk_1;
+
+alter Table person add FOREIGN KEY (dept_id) REFERENCES dept(id) on delete CASCADE on update CASCADE;
+
+update dept set id = 7 where id = 3;
+
+alter Table person add FOREIGN KEY (dept_id) REFERENCES dept(id) on delete set NULL on update set NULL;
+
+update dept set id = 3 where id = 7;
+
+-- 多对多的关系
+CREATE table athlete(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30),
+  age TINYINT NOT NULL,
+  country VARCHAR(30) NOT NULL
+);
+create Table sports(
+  id int PRIMARY KEY AUTO_INCREMENT,
+  sport VARCHAR(30) NOT NULL
+);
+CREATE Table athlete_sports (
+  id int PRIMARY key AUTO_INCREMENT,
+  aid int,
+  sid int,
+  FOREIGN KEY (aid) REFERENCES athlete(id),
+  FOREIGN KEY (sid) REFERENCES sports(id)
+);
+ALTER Table athlete_sports add rangking int after sid;
+
