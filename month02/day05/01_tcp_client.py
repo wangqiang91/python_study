eb18dd9fac6d8c90dc7fb1ad18c1d61e78a7ae1b
wangqiang91 <786528604@qq.com>
2023-10-27 17:37:39 +0800

    tcp

--- month02/day05/01_tcp_client.py
+++ month02/day05/01_tcp_client.py
@@ -1,9 +1,14 @@
 from socket import *
+
 tcp_client = socket()
 addr_server = ("127.0.0.1",8888)
 tcp_client.connect(addr_server)
-msg = input(">>")
-tcp_client.send(msg.encode())
-data = tcp_client.recv(1024)
-print("from server",data.decode())
+while True:
+    msg = input(">>")
+    tcp_client.send(msg.encode())
+    if msg == "##":
+        break
+    else:        
+        data = tcp_client.recv(1024)
+        print("from server",data.decode())
 tcp_client.close()
