6399ddc73de915446ced5ca32a7dd8d59e0d1c40
wangqiang91 <786528604@qq.com>
2023-11-10 17:03:14 +0800

    thread

---
+++ month02/day08_聊天室、thread/01_thread.py
@@ -0,0 +1,18 @@
+import os
+from threading import Thread
+from time import sleep
+a = 1
+def music():
+    for i in range(3):
+        sleep(2)
+        print(os.getpid(),"播放音乐01")
+    global a
+    print("a:",a)
+    a = 10000
+t = Thread(target=music)
+t.start()
+for i in range(4):
+        sleep(1)
+        print(os.getpid(),"播放音乐02")
+t.join()
+print("a的最终值：",a)
