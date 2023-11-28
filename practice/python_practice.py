2238578460f5bd27a91b2bc7ba79b233f0380aef
wangqiang91 <786528604@qq.com>
2023-11-28 14:16:02 +0800

    practice

--- practice/python_practice.py
+++ practice/python_practice.py
@@ -84,6 +84,93 @@ def n10():
     data = input(">>").split(" ")
     data = list(set(data))
     print(" ".join(sorted(data)))
-    
+
+# 输入逗号分割的二进制数，输出可被5整除的二进制数，以逗号分割；
+def n11():
+    data = input(">>").split(",")
+    list_data = []
+    for item in data:
+        if int(item,2) % 5 == 0:
+            list_data.append(item)
+    print(",".join(list_data))
+
+def n12():
+    values = []
+    for i in range(1000, 3001):
+        s = str(i)
+        if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
+            values.append(s)
+    print (",".join(values))
+
+def n13():
+    str_data = input(">>")
+    num = re.findall('[0-9]',str_data)
+    alp = re.findall('[a-zA-Z]',str_data)
+    print("字母",len(alp))
+    print("数字",len(num))
+
+def n14():
+    str_data = input(">>")
+    lower = re.findall('[a-z]',str_data)
+    upper = re.findall('[A-Z]',str_data)
+    print("大写实例",len(upper))
+    print("小写实例",len(lower))
+
+def n15():
+    num = input(">>")
+    total = int(num) + int(num*2) + int(num*3) + int(num*4)
+    print(total)
+
+def n16():
+    data = input(">>").split(",")
+    data_list = [x for x in data if int(x) % 2 != 0]
+    print(",".join(data_list))
+
+def n17():
+    sum = 0
+    while True:
+        data = input(">>")
+        num = data.split(" ")
+        if not data :
+            break
+        if num[0] == "D":
+            sum += int(num[1])
+        else:
+            sum -= int(num[1])
+    print(sum)
+
+def n18():
+    data = input(">>").split(",")
+    pwd = []
+    for item in data:
+        if len(item) < 6 or len(item) > 12:
+            continue
+        if len(re.findall('[a-z]',item)) > 0 and len(re.findall('[A-Z]',item)) > 0 and len(re.findall('[0-9]',item)) > 0 and len(re.findall('[$#@]',item)) > 0:
+            pwd.append(item)
+    print(",".join(pwd))
+
+def n19():
+    from operator import itemgetter
+    l = []
+    print("请输入：")
+    while True:
+        s = input()
+        if not s:
+            break
+        l.append(tuple(s.split(",")))
+    print (sorted(l, key=itemgetter(0,1,2)))
+
+# 使用生成器定义一个类，该生成器可以在给定范围0和n之间迭代可被7整除的数字
+def n20(n):
+    data = 0
+    while data < n:
+        if data % 7 == 0:
+            yield data
+        data += 1
+for i in n20(100):
+    print(i)
+
 if __name__ == "__main__":
-    n10()
+    for i in n20(100):
+        print(i)
+
